5ea79d89c6154d45140ba37fedf88a9ac939e333
AbhishekKr <abhikumar163@gmail.com>
2018-07-15 17:02:57 +0530

    first-godot-game to have colliding objects and gravity
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- first-godot-game/KinematicBody2D.gd
+++ first-godot-game/KinematicBody2D.gd
@@ -6,6 +6,8 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+	motion.y += 5
+
 	# mouse movement
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
--- first-godot-game/cat.gd
+++ first-godot-game/cat.gd
@@ -6,6 +6,8 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+	motion.y += 5
+
 	# cat movement
 	if Input.is_action_pressed("ui_up"):
 		motion.x = 100
