b73ad3188e55233b0032e8a5f0c6740e0c2f34d0
AbhishekKr <abhikumar163@gmail.com>
2018-07-15 16:53:44 +0530

    first-godot-game to have a brick at bottom and cat above
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ first-godot-game/cat.gd
@@ -0,0 +1,20 @@
+## for sprite of cat
+
+extends KinematicBody2D
+
+# class variables
+var motion = Vector2()
+
+func _physics_process(delta):
+	# cat movement
+	if Input.is_action_pressed("ui_up"):
+		motion.x = 100
+	elif Input.is_action_pressed("ui_down"):
+		motion.x = -100
+
+	if Input.is_action_just_released("ui_up"):
+		motion.x = 0
+	elif Input.is_action_just_released("ui_down"):
+		motion.x = 0
+
+	move_and_slide(motion)
