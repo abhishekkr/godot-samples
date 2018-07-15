3a4cb58335721a78e25e660669215aae267736b1
AbhishekKr <abhikumar163@gmail.com>
2018-07-15 16:07:14 +0530

    godot-samples init, first game sample stage-1 added
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## godot-samples
+---
+
+
+
---
+++ first-godot-game/KinematicBody2D.gd
@@ -0,0 +1,33 @@
+## for sprite of mouse with cheese
+
+extends KinematicBody2D
+
+# class variables
+var motion = Vector2()
+
+func _physics_process(delta):
+	# mouse movement
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+
+	if Input.is_action_pressed("ui_up"):
+		motion.y = -100
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = 100
+
+	if Input.is_action_just_released("ui_right"):
+		motion.x = 0
+	elif Input.is_action_just_released("ui_left"):
+		motion.x = 0
+
+	if Input.is_action_just_released("ui_up"):
+		motion.y = 0
+	elif Input.is_action_just_released("ui_down"):
+		motion.y = 0
+
+	move_and_slide(motion)
+
+func _ready():
+	pass
