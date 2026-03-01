58b82e47bb64cbf0ce33c5e923297679eb46af06
AbhishekKr <abhikumar163@gmail.com>
2026-03-01 21:48:37 +0530

    init 3D Creepers Dodgers from godot official doc upto spawning mob
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## godot-samples
 
 * [Creeper Dodger](https://github.com/abhishekkr/godot-creeper-dodger): following 2d game from official doc
+* [Creeper Dodger 3D](creeper-doder-3d/): following 3d game from [official doc](https://docs.godotengine.org/en/stable/getting_started/first_3d_game/index.html)
 
 * [Road Hoppers](./road-hopper): 2D game on `crossing the road` from [ClearCode's YT tutorial](https://www.youtube.com/watch?v=TLG2yVpLDT8)
 
---
+++ creeper-doder-3d/scenes/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	# We create a local variable to store the input direction.
+	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_backward"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		$Pivot.basis = Basis.looking_at(direction)
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		print("not on floor")
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
