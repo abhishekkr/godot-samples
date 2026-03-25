7e7bd8810610a48f243ae3b942d750fb894bfdca
AbhishekKr <abhikumar163@gmail.com>
2026-03-26 01:53:45 +0530

    init GDQuest 2D Vampire Survival Style tutorial upto 43min for Player with YSort
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ gdquest-vampire2d/world/characters/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@onready var player_body = %HappyBoo
+
+const SPEED = 300.0
+
+func _ready() -> void:
+	player_body.play_idle_animation()
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if velocity.length() > 0.0:
+		player_body.play_walk_animation()
+		move_and_slide()
+	else:
+		player_body.play_idle_animation()
